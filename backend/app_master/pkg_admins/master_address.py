6642e5fb1d4239b02c19a7d16fccc379f545d9b7
Akash Sengupta <52875362+akash-sgta@users.noreply.github.com>
2024-02-28 19:05:07 +0530

    initial commit 00011 - push check

---
+++ backend/app_master/pkg_admins/master_address.py
@@ -0,0 +1,37 @@
+# ========================================================================
+from utility.abstract_admin import Change_Log
+from app_master.pkg_models.master_address import ADDRESS
+
+
+# ========================================================================
+class Address(Change_Log):
+    list_display = (
+        "id",
+        "country_name",
+        "state_name",
+        "city_name",
+        "postal_code",
+        "latitude",
+        "longitude",
+    ) + super().list_display
+    list_filter = [
+        "city__state__country__eng_name",
+        "city__state__eng_name",
+        "city__eng_name",
+    ]
+    search_fields = ("id__icontains",)
+
+    def country_name(self, obj):
+        return "{}".format(obj.city.state.country.eng_name)
+
+    def state_name(self, obj):
+        return "{}".format(obj.city.state.eng_name)
+
+    def city_name(self, obj):
+        return "{}".format(obj.city.eng_name)
+
+    def created(self, obj):
+        return super().created(ADDRESS.objects.get(id=obj.id))
+
+    def changed(self, obj):
+        return super().changed(ADDRESS.objects.get(id=obj.id))
---
+++ backend/app_master/pkg_admins/master_credential.py
@@ -0,0 +1,21 @@
+# ========================================================================
+from utility.abstract_admin import Change_Log
+from app_master.pkg_models.master_credential import CREDENTIAL
+
+
+# ========================================================================
+class Text(Change_Log):
+    list_display = (
+        "id",
+        "lang_name",
+    ) + super().list_display
+    search_fields = ("id__icontains",)
+
+    def lang_name(self, obj):
+        return "{}_{}".format(obj.lang.eng_name, obj.lang.local_name)
+
+    def created(self, obj):
+        return super().created(TEXT.objects.get(id=obj.id))
+
+    def changed(self, obj):
+        return super().changed(TEXT.objects.get(id=obj.id))
